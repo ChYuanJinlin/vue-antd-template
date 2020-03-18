90319a015255eb8034b88a636ffd801ecf7b5237
1075360356@qq.com <1075360356@qq.com>
2020-03-18 16:53:48 +0800

    修改

--- src/request/index.js
+++ src/request/index.js
@@ -5,7 +5,7 @@ export default {
     // 获取轮播图
     getBanner(typeid, menuid, loc) {
         /**
-         * @param ajax(url,data[,loading],method(默认值get))
+         * @param ajax(url,data[,loading],method='get')
          */
         return ajax('banner_imgs/list', { typeid, menuid, loc },{},'post')
     },
